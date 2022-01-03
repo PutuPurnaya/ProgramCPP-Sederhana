b272cc212156fa1a70c99014bcfe686f6ac33039
I Gede Putu Purnaya <75591400+PutuPurnaya@users.noreply.github.com>
2022-01-03 09:48:18 +0800

    Add files via upload

---
+++ Program Bilangan Ganjil Genap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, i;
+		
+	for (i=0; i<50, i++)	
+	if(a%2==0){
+	cout<<"Bilangan adalah genap"<<endl;
+	
+	}else {
+	cout<<"Bilangan adalah ganjil"<<endl;
+	}
+	
+	return 0;
+}
